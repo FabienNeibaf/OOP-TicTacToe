1ea9f64730031e3bb1a902f47a65ada2fa874261
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2019-04-08 12:51:06 +0300

    Create the Game class with some methods

--- lib/logic.rb
+++ lib/logic.rb
@@ -23,3 +23,26 @@ class Move
     @track
   end
 end
+
+class Game
+  WIN_POS = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+  def initialize
+    @players = {}
+    @moves = 0
+    @board = Board.new
+  end
+  def add_player(role, name)
+    return false if @players[role]
+    @players[role] = Player.new(name)
+    @current_role = 1 if role == 1
+    return true
+  end
+  def next_move(cell)
+    return -1 unless pos(cell)
+    return 0 unless @board.fill(sym(@current_role), pos(cell))
+    @players[@current_role].next_move(pos(cell))
+    @moves += 1
+    check_winner
+    @current_role = @current_role == 1 ? 2 : 1
+  end
+end
