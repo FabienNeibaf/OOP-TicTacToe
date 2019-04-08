693a21f8ba055bfd87dcef0d500bcab795469fe0
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2019-04-08 13:22:32 +0300

    Add checking winner on Game

--- lib/logic.rb
+++ lib/logic.rb
@@ -49,14 +49,7 @@ class Game
   end
 
   def start
-    puts <<-HEREDOC
-    \t\t\t\t ********************************
-    \t\t\t\t *       TIC-TAC-TOE GAME       *
-    \t\t\t\t * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *
-    \t\t\t\t *        FABIEN/DARSHAN        *
-    \t\t\t\t ********************************
-    HEREDOC
-    puts "\n"
+    @board.credit
     @board.show
     puts "\n\n"
     print "--> Player 1: "
@@ -75,96 +68,38 @@ class Game
   end
 
   def restart
-      initialize
-      start
+    initialize
+    start
   end
 
   def stop
-      puts "\n\t\t\t\t----> See you next time! <----\n"; exit
+    puts "\n\t\t\t\t----> See you next time! <----\n"; exit
   end
 
   private
   def sym(role)
-      case role
-      when 1
-        :X
-      when 2
-        :O
-      else
-        false
-      end
+    case role
+    when 1
+      :X
+    when 2
+      :O
+    else
+      false
+    end
   end
-  
-  def pos(cell)
-      case cell.downcase
-      when 'a1','1a'; 1
-      when 'a2','2a'; 4
-      when 'a3','3a'; 7
-      when 'b1','1b'; 2
-      when 'b2','2b'; 5
-      when 'b3','3b'; 8
-      when 'c1','1c'; 3
-      when 'c2','2c'; 6
-      when 'c3','3c'; 9
-      else false
-      end
 
-      def start
-        puts <<-HEREDOC
-        \t\t\t\t ********************************
-        \t\t\t\t *       TIC-TAC-TOE GAME       *
-        \t\t\t\t * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *
-        \t\t\t\t *        FABIEN/DARSHAN        *
-        \t\t\t\t ********************************
-        HEREDOC
-        puts "\n"
-        @board.show
-        puts "\n\n"
-        print "--> Player 1: "
-        add_player(1, gets.chomp)
-        print "\n--> Player 2: "
-        add_player(2, gets.chomp)
-        puts '---------------------'
-        loop do
-          print "\n#{@players[@current_role].name} move --> "
-          input = gets.chomp
-          case next_move(input)
-          when -1; puts "\n\t!! #{input} is invalid move !!"
-          when 0; puts"\n\t!! #{input} is already filled !!"
-          end
-        end
-      end
-      def restart
-        initialize
-        start
-      end
-      def stop
-        puts "\n\t\t\t\t----> See you next time! <----\n"; exit
-      end
-      private
-      def sym(role)
-        case role
-        when 1
-          :X
-        when 2
-          :O
-        else
-          false
-        end
-      end
-      def pos(cell)
-        case cell.downcase
-        when 'a1','1a'; 1
-        when 'a2','2a'; 4
-        when 'a3','3a'; 7
-        when 'b1','1b'; 2
-        when 'b2','2b'; 5
-        when 'b3','3b'; 8
-        when 'c1','1c'; 3
-        when 'c2','2c'; 6
-        when 'c3','3c'; 9
-        else false
-        end
-      end
+  def pos(cell)
+    case cell.downcase
+    when 'a1','1a'; 1
+    when 'a2','2a'; 4
+    when 'a3','3a'; 7
+    when 'b1','1b'; 2
+    when 'b2','2b'; 5
+    when 'b3','3b'; 8
+    when 'c1','1c'; 3
+    when 'c2','2c'; 6
+    when 'c3','3c'; 9
+    else false
+    end
   end
 end
