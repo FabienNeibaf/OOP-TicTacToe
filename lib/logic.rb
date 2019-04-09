da244f69efc2112b7c25a9135396ab86aff841c9
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2019-04-09 20:44:43 +0300

    Polish codes

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,73 +1,84 @@
 require_relative "./ui.rb"
 
+# LOGIC
+# Store the player informations
 class Player
   attr_reader :name
   def initialize(name)
     @name = name
     @moves = Move.new
   end
+
   def next_move(pos)
     @moves.next(pos)
   end
+
   def moves
     @moves.get
   end
 end
 
+# Track the players move
 class Move
   def initialize
     @track = []
   end
+
   def next(pos)
     @track.push(pos)
   end
+
   def get
     @track
   end
 end
 
+# Represent the game functionality
 class Game
-  WIN_POS = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+  WIN_POS = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+    [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]
+  ].freeze
   def initialize
     @players = {}
     @moves = 0
-    @board = Board.new
+    @ui = UI.new(self)
   end
 
   def add_player(role, name)
     return false if @players[role]
+
     @players[role] = Player.new(name)
     @current_role = 1 if role == 1
-    return true
+    true
+  end
+
+  def current_player
+    @players[@current_role]
   end
 
   def next_move(cell)
-    return -1 unless pos(cell)
-    return 0 unless @board.fill(sym(@current_role), pos(cell))
+    return :wrong unless pos(cell)
+
+    return :filled unless @ui.fill(sym(@current_role), pos(cell))
+
     @players[@current_role].next_move(pos(cell))
     @moves += 1
-    check_winner
+    end? = end_game?(check_winner)
     @current_role = @current_role == 1 ? 2 : 1
+    end?
+  end
+
+  def check_winner
+    WIN_POS.each do |item|
+      win = (item & @players[@current_role].moves).length == 3
+      return @players[@current_role] if win
+    end
+    nil
   end
 
   def start
-    @board.credit
-    @board.show
-    puts "\n\n"
-    print "--> Player 1: "
-    add_player(1, gets.chomp)
-    print "\n--> Player 2: "
-    add_player(2, gets.chomp)
-    puts '---------------------'
-    puts "\n Choose cell to fill using colum_index as a1 or 1a | b2 or 2b, ..."
-    loop do
-      print "\n#{@players[@current_role].name} move --> "
-      input = gets.chomp
-      case next_move(input)
-      when -1; puts "\n\t!! #{input} is invalid move !!"
-      when 0; puts"\n\t!! #{input} is already filled !!"
-      end
-    end
+    @ui.start
   end
 
   def restart
@@ -76,50 +87,38 @@ class Game
   end
 
   def stop
-    puts "\n\t\t\t\t----> See you next time! <----\n"; exit
+    puts "\n\t\t\t\t----> See you next time! <----\n"
+    exit
+  end
+
+  def end_game?(winner)
+    if winner
+      @ui.winner
+      return winner
+    end
+    if @moves == 9
+      @ui.draw
+      return :draw
+    end
+    false
   end
 
   private
+
   def sym(role)
     case role
     when 1
       :X
     when 2
       :O
-    else
-      false
     end
   end
 
   def pos(cell)
-    case cell.downcase
-    when 'a1','1a'; 1
-    when 'a2','2a'; 4
-    when 'a3','3a'; 7
-    when 'b1','1b'; 2
-    when 'b2','2b'; 5
-    when 'b3','3b'; 8
-    when 'c1','1c'; 3
-    when 'c2','2c'; 6
-    when 'c3','3c'; 9
-    else false
-    end
-  end
-
-  def check_winner
-    WIN_POS.each { |item|
-      win = (item&@players[@current_role].moves).length == 3
-      draw = @moves == 9
-      puts "\n\t\t\t\t*************** #{@players[@current_role].name} wins! ***************" if win
-      puts "\n    \t\t\t~~ Draw game! It was a very tight battle. ~~" if draw
-      if win || draw
-        print "\n Do you want to play again? (y if yes): "
-        if gets.chomp.downcase == 'y'
-          restart
-        else
-          stop
-        end
-      end
-    }
+    {
+      'a1' => 1, '1a' => 1, 'a2' => 4, '2a' => 4, 'a3' => 7, '3a' => 7,
+      'b1' => 2, '1b' => 2, 'b2' => 5, '2b' => 5, 'b3' => 8, '3b' => 8,
+      'c1' => 3, '1c' => 3, 'c2' => 6, '2c' => 6, 'c3' => 9, '3c' => 9
+    }[cell]
   end
 end
--- lib/ui.rb
+++ lib/ui.rb
@@ -1,13 +1,78 @@
+# UI
+class UI
+  def initialize(game)
+    @game = game
+    @board = Board.new
+  end
+
+  def credit
+    puts <<-HEREDOC
+    \t\t\t\t ********************************
+    \t\t\t\t *       TIC-TAC-TOE GAME       *
+    \t\t\t\t * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *
+    \t\t\t\t *        FABIEN/DARSHAN        *
+    \t\t\t\t ********************************
+    HEREDOC
+  end
+
+  def start
+    credit
+    print "\n\n--> Player 1: #{@game.add_player(1, gets.chomp)}"
+    print "\n--> Player 2: #{@game.add_player(2, gets.chomp)}"
+    puts "---------------------\n"\
+      'Choose cell to fill using colum_index as a1 or 1a | b2 or 2b, ...'
+    iterate
+  end
+
+  def fill(sym, index)
+    @board.fill(sym, index)
+  end
+
+  def iterate
+    loop do
+      print "\n#{@game.current_player.name} move --> "
+      input = gets.chomp
+      case next_move(input)
+      when :wrong then puts "\n\t!! #{input} is invalid move !!"
+      when :filled then puts "\n\t!! #{input} is already filled !!"
+      end
+    end
+  end
+
+  def winner
+    puts "\n\t\t\t\t*************** #{@game.current_player.name} "\
+      'wins! ***************' if win
+      restart?
+  end
+
+  def draw
+    puts "\n    \t\t\t~~ Draw game! It was a very tight battle. ~~" if draw
+    restart?
+  end
+
+  def restart?
+    print "\n Do you want to play again? (y if yes, n if not): "
+    if gets.chomp.downcase == 'y'
+      @game.restart
+    else
+      @game.stop
+    end
+  end
+end
+
+# A grid showing the moves of players
 class Board
   def initialize
-    @items = Array.new(9, " ")
+    @items = Array.new(9, ' ')
   end
+
   def fill(sym, index)
-    return false if @items[index-1].is_a? Symbol
-    @items[index-1] = sym
-    show
+    return false if @items[index - 1].is_a? Symbol
+
+    @items[index - 1] = sym
     true
   end
+
   def show
     puts <<-HEREDOC
     \t\t\t\t\t     A   B   C
@@ -19,15 +84,5 @@ class Board
     \t\t\t\t\t 3 | #{@items[6]} | #{@items[7]} | #{@items[8]} |
     \t\t\t\t\t   -------------
     HEREDOC
-  end
-  def credit
-    puts <<-HEREDOC
-    \t\t\t\t ********************************
-    \t\t\t\t *       TIC-TAC-TOE GAME       *
-    \t\t\t\t * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *
-    \t\t\t\t *        FABIEN/DARSHAN        *
-    \t\t\t\t ********************************
-    HEREDOC
-    puts "\n"
   end
 end
